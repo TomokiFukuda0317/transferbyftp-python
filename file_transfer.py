060d55f0d837fc09d85bb826e2c9bdd51d783236
福田倫基 <tomoki@tomoki-3.local>
2019-12-21 19:14:15 +0900

    fix

--- file_transfer.py
+++ file_transfer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import paramiko
 import sys
+import zipfile,os
 import logging
 import datetime
 logging.basicConfig(filename='log/file_transfer.log', level=logging.DEBUG)
@@ -21,9 +22,21 @@ args = sys.argv
 
 current_time = datetime.datetime.today()
 print("現在時刻："+str(current_time))
-
 print("転送ファイル名：" + args[1])
-local_path = args[1]
+#os.chdir('/Users/tomoki/dev2')
+zip_f_name = str(args[1])+".zip"
+
+# 圧縮するファイルのディレクトリへ移動
+#os.chdir('C:\\user1\\data')
+
+# ZIPファイルを作成/ZIPファイルに追加
+with zipfile.ZipFile(zip_f_name,'w') as zip_f:
+    zip_f.write(args[1], compress_type=zipfile.ZIP_DEFLATED)
+
+
+print(zip_f_name)
+
+local_path = zip_f_name
 remote_path = "/home/vagrant/files/"+current_time.strftime("%Y%m%d_%H%M%S")+"/"
 
 
@@ -37,7 +50,7 @@ def main(local_file,remote_file):
         client.connect(HOST, PORT, USER, pkey=rsa_key) # キーを指定することでパスワードは必要なし
         sftp = client.open_sftp()
         sftp.mkdir(remote_path)
-        sftp.put(local_file,remote_file+args[1])
+        sftp.put(local_file,remote_file+zip_f_name)
         print("転送完了")
 
     except Exception as e:
