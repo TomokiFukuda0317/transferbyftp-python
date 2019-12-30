61771639665edbb33dc0fc28348d902be58c4c04
福田倫基 <tomoki@tomoki-3.local>
2019-12-30 11:29:25 +0900

    fix

--- file_transfer.py
+++ file_transfer.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 import paramiko
 import sys
-import zipfile,os
+import zipfile
 import logging
 import datetime
+import random
 logging.basicConfig(filename='log/file_transfer.log', level=logging.DEBUG)
 
 HOST = '153.126.154.31'
-PORT = 22 # SFTPのポート
-USER = 'ftp-user'
-PASSWORD = "password1"
-# KEY_FILE = '/Users/tomoki/.ssh/id_rsa_ftp'
-# KEY_FILE = '/Users/tomoki/vagrant/ubunt_server/.vagrant/machines/default/virtualbox/private_key' # 秘密鍵ファイル
+PORT = 22
+USER = 'ubuntu'
+KEY_FILE = '/Users/tomoki/.ssh/id_rsa_ftp'
 
 # 秘密鍵ファイルからキーを取得
-# rsa_key = paramiko.RSAKey.from_private_key_file(KEY_FILE)
+rsa_key = paramiko.RSAKey.from_private_key_file(KEY_FILE)
 
 # 秘密鍵ファイルにパスフレーズを設定している場合は下記
 # PASSPHRASE = 'passphrase'
@@ -25,21 +24,18 @@ args = sys.argv
 current_time = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
 print("現在時刻："+current_time)
 print("転送ファイル名：" + args[1])
-#os.chdir('/Users/tomoki/dev2')
-zip_file = "file"+current_time+".zip"
-
-# 圧縮するファイルのディレクトリへ移動
-#os.chdir('C:\\user1\\data')
+tmp = str(random.random())
+zip_file = "file"+current_time+"_"+tmp+".zip"
 
 # ZIPファイルを作成/ZIPファイルに追加
-with zipfile.ZipFile(zip_file,'w') as tmp:
-    tmp.write(args[1], compress_type=zipfile.ZIP_DEFLATED)
+with zipfile.ZipFile(zip_file,'w',zipfile.ZIP_DEFLATED) as tmp:
+    tmp.write(args[1])
 
 
 print(zip_file)
 
 local_path = zip_file
-remote_path = "/var/www/ftp_dir"
+remote_path = "/home/ubuntu/files/"+current_time
 
 
 def main(local_file,remote_file):
@@ -49,9 +45,9 @@ def main(local_file,remote_file):
     try:
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(HOST, PORT, USER, PASSWORD) # キーを指定することでパスワードは必要なしpkey=rsa_key
+        client.connect(HOST, PORT, USER, pkey=rsa_key) # キーを指定することでパスワードは必要なし
         sftp = client.open_sftp()
-        #sftp.mkdir(remote_path)
+        sftp.mkdir(remote_path)
         sftp.put(local_file,remote_file+"/"+zip_file)
         print("転送完了")
 
