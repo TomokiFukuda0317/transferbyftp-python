d52146612144594ab8203c6e6dbda690416913ad
福田倫基 <tomoki@tomoki-3.local>
2019-12-31 12:59:37 +0900

    done

--- dirwatch.py
+++ dirwatch.py
@@ -9,10 +9,10 @@ from __future__ import print_function
 import sys
 import time
 import subprocess
+import os
+import zipfile
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-
-
 
 class MyHandler(PatternMatchingEventHandler):
     def __init__(self, patterns):
@@ -20,28 +20,29 @@ class MyHandler(PatternMatchingEventHandler):
 
     def _run_command(self):
         # ファイル更新をトリガーに実行するpythonを記述
-        print("ファイルに更新がありました。")
+        print("Hello World!!")
 
     def on_created(self, event):
-        # filepath = event.src_path
-        # filename = os.path.basename(filepath)
-        # print(filepath)
-        # if zipfile.is_zipfile(filepath):
-        # 解凍
-        #     with zipfile.ZipFile(filename,'r') as zf:
-        #         zf.extractall(target_dir)
-        #     print('Zipファイルを解凍しました')
-        # else:
-        #     print('%sができました' % filename)
-        
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        file_save_dir = "/home/ubuntu/files/." # 解凍したファイルの保存先
+
+        print("クライアントからzipファイルを受信しました")
+        print(filepath)
+
+        with zipfile.ZipFile(filepath,'r') as zf:
+            zf.extractall(file_save_dir)
+        print(file_save_dir+"へzipファイルを解凍しました")
+
         self._run_command()
 
-# ファイル監視の開始
+# ファイル監視の開始（watchdogを使用）
 def watch(target_dir, extension):
     event_handler = MyHandler(extension)
     observer = Observer()
     observer.schedule(event_handler, target_dir, recursive=True)
     observer.start()
+
     # 処理が終了しないようスリープを挟んで無限ループ
     try:
         while True:
@@ -49,7 +50,6 @@ def watch(target_dir, extension):
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
-
 
 if __name__ == "__main__":
     if 2 > len(sys.argv):
--- file_transfer.py
+++ file_transfer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+"""
+    実行コマンド　
+    $ python file_transfer.py　${対象のcsvファイル}
+"""
+
 import paramiko
 import sys
 import zipfile
 import logging
 import datetime
 import random
+import os
 logging.basicConfig(filename='log/file_transfer.log', level=logging.DEBUG)
 
 HOST = '153.126.154.31'
@@ -21,22 +27,26 @@ rsa_key = paramiko.RSAKey.from_private_key_file(KEY_FILE)
 
 args = sys.argv
 
-current_time = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
+current_time = datetime.datetime.today().strftime("%Y%m%d-%H%M%S")
 print("現在時刻："+current_time)
-print("転送ファイル名：" + args[1])
+print("対象ファイル名：" + args[1])
 tmp = str(random.random())
-zip_file = "file"+current_time+"_"+tmp+".zip"
+
+# 対N通信を想定し、ファイル名が被らないよう日付+乱数でユニークにする
+file_name = "file"+current_time+"-"+tmp
+zip_file = file_name+".zip"
+
+# 解凍後のファイル名
+unzip_file_name = args[1].strip(".csv")+"_"+file_name+".csv"
 
 # ZIPファイルを作成/ZIPファイルに追加
 with zipfile.ZipFile(zip_file,'w',zipfile.ZIP_DEFLATED) as tmp:
-    tmp.write(args[1])
-
-
-print(zip_file)
+    tmp.write(args[1],unzip_file_name)
+print("対象ファイルをzip化してFTPサーバへ転送します："+zip_file)
 
 local_path = zip_file
+# 転送先サーバ側のディレクトリを設定
 remote_path = "/home/ubuntu/files/"+current_time
-
 
 def main(local_file,remote_file):
     """
@@ -45,11 +55,11 @@ def main(local_file,remote_file):
     try:
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(HOST, PORT, USER, pkey=rsa_key) # キーを指定することでパスワードは必要なし
+        client.connect(HOST, PORT, USER, pkey=rsa_key) 
         sftp = client.open_sftp()
         sftp.mkdir(remote_path)
         sftp.put(local_file,remote_file+"/"+zip_file)
-        print("転送完了")
+        print("転送完了しました")
 
     except Exception as e:
         logging.error(e)
@@ -59,6 +69,7 @@ def main(local_file,remote_file):
     finally:
         sftp.close()
         client.close()
+        os.remove(local_file)
 
 if __name__ == "__main__":
     main(local_path,remote_path)
