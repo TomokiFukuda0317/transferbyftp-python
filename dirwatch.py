423a5755faf3a9ef967719c907399be249d67cc4
福田倫基 <tomoki@tomoki-3.local>
2020-01-13 13:41:29 +0900

    fix

--- dirwatch.py
+++ dirwatch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
     実行コマンド　
-    $ python dirwatch.py　${監視対象のディレクトリ}
+    $ python dirwatch.py　-d ${監視対象のディレクトリ}
 """
 
 from __future__ import print_function
@@ -11,8 +11,24 @@ import time
 import subprocess
 import os
 import zipfile
+import argparse
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+
+# パーサーを作る
+parser = argparse.ArgumentParser(
+            prog='dirwatch', # プログラム名
+            usage='ディレクトリを監視し、ファイル転送をトリガーに任意の処理を実行する', # プログラムの利用方法
+            description='description', # 引数のヘルプの前に表示
+            epilog='end', # 引数のヘルプの後で表示
+            add_help=True, # -h/–help オプションの追加
+            )
+ 
+# 引数の追加
+parser.add_argument('-d', '--watch_dir', help='監視するディレクトリを指定する', required=True)
+
+# 引数を解析する
+args = parser.parse_args()
 
 class MyHandler(PatternMatchingEventHandler):
     def __init__(self, patterns):
@@ -55,4 +71,4 @@ if __name__ == "__main__":
     if 2 > len(sys.argv):
         print("監視対象のディレクトリを第１引数に設定してください")
     else:
-        watch(sys.argv[1], ["*zip"])
+        watch(args.watch_dir, ["*zip"])
